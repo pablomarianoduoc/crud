6b64c40d595d19ec31ec9265dc38df1dc484b9c4
pablomariano <pablomariano@gmail.com>
2023-06-01 04:25:56 -0400

    Funciones agregar y eliminar implementadas

--- Personas/Aplicaciones/Alumnos/models.py
+++ Personas/Aplicaciones/Alumnos/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Alumno(models.Model):
     rut = models.CharField(primary_key=True,max_length=10)
     nombre = models.CharField(max_length=60)
+
+    def __str__(self):
+        texto = "{0} - {1}"
+        return texto.format(self.rut, self.nombre)
+
--- Personas/Aplicaciones/Alumnos/views.py
+++ Personas/Aplicaciones/Alumnos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Alumno
 
 # Create your views here.
+
+def home(request):
+    alumnosListados = Alumno.objects.all()
+    return render(request, "gestionAlumnos.html", {"alumnos": alumnosListados})
+
+def registrarAlumno(request):
+    rut=request.POST['txtRut']
+    nombre=request.POST['txtNombre']
+
+    alumno = Alumno.objects.create(rut=rut, nombre=nombre)
+    return redirect('/')
+
+def eliminacionAlumno(request, rut):
+    alumno = Alumno.objects.get(rut=rut)
+    alumno.delete()
+
+    return redirect('/')
